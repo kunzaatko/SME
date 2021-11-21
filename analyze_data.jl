fc428df542d72c8a87acf285a142f938ae811da7
Martin Kunz <martinkunz@email.cz>
2021-11-22 00:57:05 +0100

    add: tests of independence of categories

--- analyze_data.jl
+++ analyze_data.jl
@@ -1,9 +1,23 @@
+using HypothesisTests
+
 #' @description
 #'
 #' Shows all the freqtables for the data
 function show_freqtables()
     # {{{
-    freq_tables = include("./freq_tables.jl")
+    include("./freq_tables.jl")
+
+    eagle_freq = get_prop_freq_tables(eagle, :CrustDescription, :Topping; sums=true)
+    dominos_freq = get_prop_freq_tables(dominos, :CrustDescription, :Topping; sums=true)
+    crust_freq = get_prop_freq_tables(data, :Store, :Crust; sums=true)
+    top_freq = get_prop_freq_tables(data, :Store, :Top; sums=true)
+
+    freq_tables = Dict(
+        :eagle_boys => eagle_freq,
+        :dominos => dominos_freq,
+        :crust => crust_freq,
+        :top => top_freq,
+    )
 
     printstyled(
         """
@@ -111,6 +125,91 @@ function show_freqtables()
         color=:blue,
     )
     return show(freq_tables[:top][:proportion_freqtable])
+end # }}}
+
+#' @description
+#'
+#' Shows hypothesis tests for independence of categories for subpopulations by stores
+function show_category_indep_hyp_tests()
+    # {{{
+    include("./freq_tables.jl")
+    eagle_freq = get_prop_freq_tables(eagle, :CrustDescription, :Topping)
+    dominos_freq = get_prop_freq_tables(dominos, :CrustDescription, :Topping)
+    eagle_freq_sums = get_prop_freq_tables(eagle, :CrustDescription, :Topping; sums=true)
+    dominos_freq_sums = get_prop_freq_tables(
+        dominos, :CrustDescription, :Topping; sums=true
+    )
+
+    indep_eagle = ChisqTest(eagle_freq[:freqtable])
+    indep_dominos = ChisqTest(dominos_freq[:freqtable])
+
+    printstyled(
+        """
+#######################################################################
+#                   Testy nezávislosti kategorií                      #
+#######################################################################
+""";
+        color=:red,
+    )
+    printstyled(
+        """
+    # EagleBoys
+""";
+        color=:cyan,
+    )
+    printstyled("\t\t", eagle_freq_sums[:proportion_freqtable], "\n\n"; color=:blue)
+    show(indep_eagle)
+
+    printstyled(
+        """\n
+    # Dominos
+""";
+        color=:cyan,
+    )
+    printstyled("\t\t", dominos_freq_sums[:proportion_freqtable], "\n\n"; color=:blue)
+    return show(indep_dominos)
+end # }}}
+
+#' @description
+#'
+#' Shows hypothesis tests for homogenity of category for subpopulation by stores
+function show_store_category_indep_hyp_tests()
+    # {{{
+    include("./freq_tables.jl")
+    crust_freq = get_prop_freq_tables(data, :Store, :Crust)
+    top_freq = get_prop_freq_tables(data, :Store, :Top)
+    crust_freq_sums = get_prop_freq_tables(data, :Store, :Crust; sums=true)
+    top_freq_sums = get_prop_freq_tables(data, :Store, :Top; sums=true)
+
+    homo_crust = ChisqTest(crust_freq[:freqtable])
+    homo_top = ChisqTest(top_freq[:freqtable])
+
+    printstyled(
+        """
+#######################################################################
+#                 Testy homogenity firmy a kategorie                  #
+#######################################################################
+""";
+        color=:red,
+    )
+
+    printstyled(
+        """
+    # Kůrka
+""";
+        color=:cyan,
+    )
+    printstyled("\t\t", crust_freq_sums[:proportion_freqtable], "\n\n"; color=:blue)
+    show(homo_crust)
+
+    printstyled(
+        """\n
+    # Přísada
+""";
+        color=:cyan,
+    )
+    printstyled("\t\t", top_freq_sums[:proportion_freqtable], "\n\n"; color=:blue)
+    return show(homo_top)
 end # }}}
 
 function show_simple_stats()
